c874a434a3aa9e4cc5e0cb2ecf54b31e920b75a4
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-08-21 18:00:11 +0100

    first version

---
+++ screens/Initial.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Initial = () => {
+  return (
+    <View>
+      <Text>Initial</Text>
+    </View>
+  )
+}
+
+export default Initial
